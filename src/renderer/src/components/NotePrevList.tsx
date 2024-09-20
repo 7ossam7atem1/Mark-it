aa6d25ce7805019b3f849fa8e746265308ca8bf9
Hossam Hatem <7ossam7atem1@gmail.com>
2024-09-21 02:43:57 +0300

    feat: handle no existing notes

--- src/renderer/src/components/NotePrevList.tsx
+++ src/renderer/src/components/NotePrevList.tsx
@@ -1,10 +1,19 @@
 import { notesMock } from '@/store/mocks'
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 import { NotePrev } from './NotePrev'
 
-export const NotePrevList = ({ ...props }: ComponentProps<'ul'>) => {
+export const NotePrevList = ({ className, ...props }: ComponentProps<'ul'>) => {
+  if (notesMock.length === 0) {
+    return (
+      <ul className={twMerge('text-center pt-4', className)} {...props}>
+        <span>No Notes Yet!</span>
+      </ul>
+    )
+  }
+
   return (
-    <ul {...props}>
+    <ul {...props} className={twMerge(className)}>
       {notesMock.map((note) => (
         <NotePrev key={note.title + note.lastEditedtime} {...note} />
       ))}
